e55728ba39723b20231db9443d4f3436e86a2681
lijok <lijok@pm.me>
2024-03-23 16:56:26 +0000

    Implement barebones CLI

---
+++ tests/smoke/tests/test_cli.py
@@ -0,0 +1,15 @@
+import subprocess
+from subprocess import CompletedProcess
+from typing import Self
+
+from icebreaker import __version__
+
+
+class TestCLIVersion:
+    def test_returns_expected_output(self: Self) -> None:
+        output: CompletedProcess = subprocess.run(["icebreaker", "version"], check=True, text=True, capture_output=True)
+        assert output.stdout == __version__
+
+    def test_returns_expected_status_code(self: Self) -> None:
+        output: CompletedProcess = subprocess.run(["icebreaker", "version"], check=True, text=True, capture_output=True)
+        assert output.returncode == 0
