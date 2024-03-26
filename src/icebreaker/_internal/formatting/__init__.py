e5334c502ca094d5a57799ff180fc7ac8fb948b3
lijok <lijok@pm.me>
2024-03-26 20:48:38 +0000

    Remove duplicate import

--- src/icebreaker/_internal/formatting/__init__.py
+++ src/icebreaker/_internal/formatting/__init__.py
@@ -3,5 +3,4 @@ from icebreaker._internal.formatting._formatter import FormatReport as FormatRep
 from icebreaker._internal.formatting._formatter import Success as Success
 from icebreaker._internal.formatting._formatter import Report as Report
 from icebreaker._internal.formatting._formatter import CheckReport as CheckReport
-from icebreaker._internal.formatting._formatter import FormatReport as FormatReport
 from icebreaker._internal.formatting._ruff_formatter import RuffFormatter as RuffFormatter
