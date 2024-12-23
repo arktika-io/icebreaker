d52843f75f0961a7fb1a71ab4ab6d2a065180af4
lijok <lijok@pm.me>
2024-12-23 11:13:02 +0000

    Split store fixtures into their own module

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -2,3 +2,4 @@ from tests.functional.fixtures.memory_store_backend import *  # noqa
 from tests.functional.fixtures.env_vars_store_backend import *  # noqa
 from tests.functional.fixtures.env_var_store_backend import *  # noqa
 from tests.functional.fixtures.store_backend import *  # noqa
+from tests.functional.fixtures.store import *  # noqa
--- tests/functional/fixtures/store_backend.py
+++ tests/functional/fixtures/store_backend.py
@@ -1,9 +1,6 @@
-from uuid import uuid4
-
 import pytest
 from pytest import FixtureRequest
 
-from icebreaker.store import Store
 from icebreaker.store_backends.protocol import Append
 from icebreaker.store_backends.protocol import Delete
 from icebreaker.store_backends.protocol import Read
@@ -88,62 +85,3 @@ def store_backend_implementing_write(request: FixtureRequest) -> Write:
 def store_backend_implementing_write_if_not_exists(request: FixtureRequest) -> WriteIfNotExists:
     store_backend: WriteIfNotExists = request.getfixturevalue(request.param)
     return store_backend
-
-
-@pytest.fixture(scope="function")
-def store_implementing_append(store_backend_implementing_append: Append) -> Store[Append]:
-    return Store(store_backend=store_backend_implementing_append)
-
-
-@pytest.fixture(scope="function")
-def store_implementing_delete(store_backend_implementing_delete: Delete) -> Store[Delete]:
-    return Store(store_backend=store_backend_implementing_delete)
-
-
-@pytest.fixture(scope="function")
-def store_implementing_read(store_backend_implementing_read: Read) -> Store[Read]:
-    return Store(store_backend=store_backend_implementing_read)
-
-
-@pytest.fixture(scope="function")
-def store_implementing_write(store_backend_implementing_write: Write) -> Store[Write]:
-    return Store(store_backend=store_backend_implementing_write)
-
-
-@pytest.fixture(scope="function")
-def store_implementing_write_if_not_exists(
-    store_backend_implementing_write_if_not_exists: WriteIfNotExists,
-) -> Store[WriteIfNotExists]:
-    return Store(store_backend=store_backend_implementing_write_if_not_exists)
-
-
-@pytest.fixture(scope="session")
-def populated_store_key() -> str:
-    return str(uuid4())
-
-
-@pytest.fixture(scope="function")
-def populated_store_implementing_read(
-    store_implementing_write: Store[Write],
-    populated_store_key: str,
-) -> Store[Read]:
-    store_implementing_write.write_string(key=populated_store_key, data="test")
-    return store_implementing_write
-
-
-@pytest.fixture(scope="function")
-def populated_store_implementing_delete(
-    store_implementing_delete: Store[Delete],
-    populated_store_key: str,
-) -> Store[Delete]:
-    store_implementing_delete.write_string(key=populated_store_key, data="test")
-    return store_implementing_delete
-
-
-@pytest.fixture(scope="function")
-def populated_store_implementing_write_if_not_exists(
-    store_implementing_write_if_not_exists: Store[WriteIfNotExists],
-    populated_store_key: str,
-) -> Store[WriteIfNotExists]:
-    store_implementing_write_if_not_exists.write_string(key=populated_store_key, data="test")
-    return store_implementing_write_if_not_exists
